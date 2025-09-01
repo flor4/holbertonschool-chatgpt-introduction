93a25eee7454e34e885c0f634ee2c74e897a4211
flor4 <salanson.flora@outlook.fr>
2025-09-01 15:51:15 +0200

    Fix: files checked with pycodestyle

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import sys
 
+
 def factorial(n):
     result = 1
     while n > 1:
@@ -8,6 +9,6 @@ def factorial(n):
         n -= 1  # Correction ici
     return result
 
+
 f = factorial(int(sys.argv[1]))
 print(f)
-
